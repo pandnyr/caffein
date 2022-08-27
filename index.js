49d718f113edc6ca4d2e82868dbecd780c5df103
pandnyr <pandnyr@hotmail.com>
2022-08-28 00:46:18 +0300

    nsfw things

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js")
 require("dotenv").config()
-const { joinVoiceChannel } = require("@discordjs/voice")
+const akaneko = require("akaneko")
 
-const ytdl = require('ytdl-core')
 
 const client = new Discord.Client({
     intents:[
@@ -10,7 +9,7 @@ const client = new Discord.Client({
         "GuildMessages",
         "MessageContent",
         "GuildMembers",
-        "GuildVoiceStates"
+        "GuildVoiceStates",
     ]
 })
 
@@ -69,7 +68,78 @@ module.exports = bot
 
 })
 
-//////////// MUSIC /////////////
+//////////// NSFW /////////////
+
+client.on("messageCreate", async message => {
+    if (!message.channel.nsfw) return message.channel.send("Sorry, Not NSFW Channel!")
+
+    const hentai = new Discord.EmbedBuilder().setTitle('NSFW')
+    const ass = new Discord.EmbedBuilder().setTitle('NSFW')
+    const cum = new Discord.EmbedBuilder().setTitle('NSFW')
+    const thighs = new Discord.EmbedBuilder().setTitle('NSFW')
+    const masturbation = new Discord.EmbedBuilder().setTitle('NSFW')
+    const gifs = new Discord.EmbedBuilder().setTitle('NSFW')
+    const bdsm = new Discord.EmbedBuilder().setTitle('NSFW')
+    const blowjob = new Discord.EmbedBuilder().setTitle('NSFW')
+    const feet = new Discord.EmbedBuilder().setTitle('NSFW')
+    const doujin = new Discord.EmbedBuilder().setTitle('NSFW')
+    const foxgirl = new Discord.EmbedBuilder().setTitle('NSFW')
+
+
+    var command = message.content.toLowerCase().slice(bot.prefix.length)
+    
+    if (!message.content.startsWith(bot.prefix) || message.author.bot) return
+
+
+
+    if (command == 'hentai'){
+        hentai.setImage(await akaneko.nsfw.hentai()).setColor('Red').setDescription("Your hentai is here!")
+        message.channel.send({embeds: [hentai]})
+    }
+    else if (command == 'ass'){
+        ass.setImage(await akaneko.nsfw.ass()).setColor('Red').setDescription("I know you like anime ass~ uwu")
+        message.channel.send({embeds: [ass]})
+    }
+    else if (command == 'cum'){
+        cum.setImage(await akaneko.nsfw.cum()).setColor('Red').setDescription("Basically sticky white stuff that is usually milked from sharpies.")
+        message.channel.send({embeds: [cum]})
+    }
+    else if (command == 'thighs'){
+        thighs.setImage(await akaneko.nsfw.thighs()).setColor('Random').setDescription("The top part of your legs, very hot, isn't it?")
+        message.channel.send({embeds: [thighs]})
+    }
+    else if (command == 'masturbation'){
+        masturbation.setImage(await akaneko.nsfw.masturbation()).setColor('Blurple').setDescription("Solo Queue in CSGO!")
+        message.channel.send({embeds: [masturbation]})
+    }
+    else if (command == 'gifs'){
+        gifs.setImage(await akaneko.nsfw.gifs()).setColor('DarkPurple').setDescription("Basically an animated image, so yes :3")
+        message.channel.send({embeds: [gifs]})
+    }
+    else if (command == 'foxgirl'){
+        foxgirl.setImage(await akaneko.nsfw.foxgirl()).setColor('DarkVividPink').setDescription("Girl's that are wannabe foxes, yes")
+        message.channel.send({embeds: [foxgirl]})
+    }
+    else if (command == 'bdsm'){
+        bdsm.setImage(await akaneko.nsfw.bdsm()).setColor('Random').setDescription("If you don't know what it is, search it up")
+        message.channel.send({embeds: [bdsm]})
+    }
+    else if (command == 'blowjob'){
+        blowjob.setImage(await akaneko.nsfw.bdsm()).setColor('Random').setDescription("Basically an image of a girl sucking on a sharp blade!")
+        message.channel.send({embeds: [blowjob]})
+    }
+    else if (command == 'doujin'){
+        doujin.setImage(await akaneko.nsfw.doujin()).setColor('Random').setDescription("It's here your doujin!")
+        message.channel.send({embeds: [doujin]})
+    }
+    else if (command == 'feet'){
+        feet.setImage(await akaneko.nsfw.feet()).setColor('Random').setDescription("So you like smelly feet huh?")
+        message.channel.send({embeds: [feet]})
+    }
+
+
+
+})
 
 //////////////////////////
 const welcomeChannelId = "1011930095507750912"
